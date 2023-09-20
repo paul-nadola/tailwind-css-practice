40e28ce70c14947b5658b8db60512beeeaa825b1
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-09-20 17:01:27 +0300

    Pricing

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,90 @@
+import { CheckIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+function AllInOne() {
+  return (
+    <div className='w-full my-32'>
+        <div className='max-w-[1240px] mx-auto px-2'>
+            <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+            <p className='text-2xl py-8 text-gray-500 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, mollitia? Vero nemo officia quae ut cum ducimus, dolor quisquam, quaerat dolores dolorem eaque maxime voluptate magnam! Natus, omnis dolorum? Repellat.</p>
+
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+            <div className='flex'>
+                <div>
+                <CheckIcon className='w-7 mr-4 text-green-600'/>
+                </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur nesciunt temporibus, perferendis perspiciatis quas reiciendis vel id delectus error esse ipsam sunt maiores tempore qui harum culpa! Laudantium, quo.</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default AllInOne
